8047fa1c03a5ab729b4b7dbf11c82072af71507d
neethu90 <neethur90@gmail.com>
2017-02-20 14:03:27 +0530

    Add files via upload

---
+++ small.java
@@ -0,0 +1,27 @@
+
+public class small {
+	public static void main(String[] args) {
+		
+		int a[]={2,6,7,1};
+	
+		int small=a[0];
+		
+		for(int i=0;i<4
+				;i++)
+			
+			
+		{
+			if(small>a[i])
+			{
+				small=a[i];
+		}
+			
+			
+			
+	}
+	System.out.println(small);
+	
+	
+	}	
+
+}
