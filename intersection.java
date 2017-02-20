d0765d2c5064fbdced95e4a3c446d5c632fae8bb
neethu90 <neethur90@gmail.com>
2017-02-20 14:02:30 +0530

    Add files via upload

---
+++ intersection.java
@@ -0,0 +1,25 @@
+
+public class intersection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// boolean flag=false;
+		int a[] = { 1, 2, 8 };
+		int b[] = { 2, 5, 3,8 };
+		//int c[] = new int[50];
+
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < b.length; j++) {
+				if (a[i] == b[j]) {
+					
+					System.out.println(a[i] );
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
